d1f44c0a4d7775ff6ca4cc354d1ded2bfcc53f83
Fabrizio Iozzi <fabrizio.iozzi@gmail.com>
2022-01-05 00:13:43 +0100

    with exceptions

--- 2022/io/readfile.py
+++ 2022/io/readfile.py
@@ -27,6 +27,7 @@ def print_file_2(filename):
 filename = 'Artists.txt'
 
 print_file(filename, nlines=4)
+# print_file_2(filename)
 
 import csv
 
@@ -41,3 +42,25 @@ with open(filename, newline='', encoding='utf-8') as f:
             row[4] and row[5] and row[6] and row[7] and row[8]) :
             missing_data_lines_count += 1
     print(missing_data_lines_count, data_lines_count)
+
+def longest_string(filename, col, refcol, delimiter=',', quotechar='"', sign=1):
+    # refcol is supposed to be unique
+    with open(filename,'r') as f:
+        linereader = csv.reader(f, delimiter=delimiter, quotechar=quotechar)
+        headers = next(linereader)
+        firstrow = next(linereader)
+        prev_val = float(firstrow[col])
+        max_variation = 0
+        max_var_ref = firstrow[refcol]
+        for row in linereader:
+            if (float(row[col]) - prev_val) * sign > 0:
+                variation = float(row[col]) - prev_val
+                if variation > max_variation:
+                    max_variation = variation
+                    max_var_ref = row[refcol]
+            prev_val = float(row[col])
+    
+    return max_var_ref, max_var_ref
+
+v, vr = longest_string('MSFT.csv',5,0)
+print (v, vr)
