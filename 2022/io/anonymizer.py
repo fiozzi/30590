5e4055e5207c83d90eda7b12b69b03add4f2542a
Fabrizio Iozzi <fabrizio.iozzi@gmail.com>
2022-01-08 21:43:49 +0100

    new anonymizer

--- 2022/io/anonymizer.py
+++ 2022/io/anonymizer.py
@@ -40,39 +40,53 @@ def anonymize(
     table_file : name of reference table (each line is "ID, removed columns")
     """
     try:
-        with open(csv_file, encoding='utf-8') as f_in, open(anonymized_file, 'w') as f_out:
+        with open(csv_file, encoding='utf-8') as f_in, open(anonymized_file, 'w', newline='') as f_out:
+            line_reader = csv.reader(f_in)
+            line_writer = csv.writer(f_out, dialect='excel')
+            cols_to_keep = []
+            if has_header:
+                # reads header in list and puts indexes in cols_to_remove.
+                # Will use cols_to_remove from here on
+                headers = next(line_reader)
+                for col in headers:
+                    if col in fields_to_remove:
+                        cols_to_remove.append(headers.index(col))
+                    else:
+                        cols_to_keep.append(headers.index(col))
+                # write new header to file
+                # print(cols_to_keep)
+                # print(cols_to_remove)
+                anon_header = [new_header]
+                [anon_header.append(headers[col]) for col in cols_to_keep]
+                line_writer.writerow(anon_header)
+                # print(anon_header)
             if build_table:
                 try:
-                    with open(table_file, 'w') as f_table:
-                        linereader = csv.reader(f_in, delimiter=sep, quotechar=quotechar)
-                        cols_to_keep = []
-                        if has_header:
-                            # reads header in list and puts indexes in cols_to_remove.
-                            # Will use cols_to_remove from here on
-                            headers = next(linereader)
-                            for col in headers:
-                                if col in fields_to_remove:
-                                    cols_to_remove.append(headers.index(col))
-                                else:
-                                    cols_to_keep.append(headers.index(col))
-                            # write new header to file
-                            print(cols_to_keep)
-                            print(cols_to_remove)
-                            anon_header = [new_header]
-                            [anon_header.append(headers[col]) for col in cols_to_remove]
-                            print(anon_header)
-                        # reading lines in file
-                        row_counter = 0
-                        for row in linereader:
-                            row_counter += 1
-                            anonymized_row = [str(row_counter)]
-                            [anonymized_row.append(row[col]) for col in cols_to_keep]
-                            print(anonymized_row)
+                    with open(table_file, 'w', newline='') as f_table:
+                        table_writer = csv.writer(f_table, dialect='excel')
+                        table_header = [new_header]
+                        [table_header.append(headers[col]) for col in cols_to_remove]
+                        table_writer.writerow(table_header)
+                        write_anonymized(line_reader,cols_to_keep, line_writer, table_writer, cols_to_remove)
                 except Exception as e:
                     print(f'Error in opening table file for writing: {e}')
+            else:
+                write_anonymized(line_reader,cols_to_keep,line_writer)
+                    
     except Exception as e:
         print('Error in opening cvs and/or anon file: {e.errno}, {e.strerror}, {e.filename}')
 
+def write_anonymized(linereader, cols_to_keep, line_writer, table_writer=None, cols_to_remove=[] ):
+    row_counter = 0
+    for row in linereader:
+        row_counter += 1
+        anonymized_row = [str(row_counter)]
+        [anonymized_row.append(row[col]) for col in cols_to_keep]
+        line_writer.writerow(anonymized_row)
+        if table_writer:
+            table_row = [str(row_counter)]
+            [table_row.append(row[col]) for col in cols_to_remove]
+            table_writer.writerow(table_row)
 
 def anonymize_nocsv(f_in, k, sep = ',', header = 'ID', f_out = 'out.txt'):
     with open(f_in,'r') as infile, open(f_out,'w') as outfile:
@@ -120,5 +134,5 @@ def anonymize2(f_in, k, sep = ',', header = 'ID', f_out = 'out.txt', t_out = 'ta
                 break
 
 if __name__ == '__main__':
-    anonymize('shortmidterm.txt',fields_to_remove=['STUDENTID','FIRSTNAME'])
+    anonymize(csv_file = 'shortmidterm.txt', fields_to_remove=['STUDENTID','FIRSTNAME'], quotechar='"', build_table=True)
     # anonymize2('midterm.txt', k=2)
